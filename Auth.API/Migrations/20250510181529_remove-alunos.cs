eb583f0d835c189be5002248b849be1ea7dacaf0
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-05-10 11:15:43 -0700

    remove alunos

---
+++ Auth.API/Migrations/20250510181529_remove-alunos.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Auth.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class removealunos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "aluno");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "aluno",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EmpresaId = table.Column<int>(type: "integer", nullable: false),
+                    EnderecoDestinoId = table.Column<int>(type: "integer", nullable: false),
+                    EnderecoPartidaId = table.Column<int>(type: "integer", nullable: false),
+                    EnderecoRetornoId = table.Column<int>(type: "integer", nullable: true),
+                    ResponsavelId = table.Column<int>(type: "integer", nullable: false),
+                    CPF = table.Column<string>(type: "text", nullable: true),
+                    Contato = table.Column<string>(type: "text", nullable: true),
+                    DataAlteracao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    PrimeiroNome = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    UltimoNome = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_aluno", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_aluno_empresa_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "empresa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_aluno_endereco_EnderecoDestinoId",
+                        column: x => x.EnderecoDestinoId,
+                        principalTable: "endereco",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_aluno_endereco_EnderecoPartidaId",
+                        column: x => x.EnderecoPartidaId,
+                        principalTable: "endereco",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_aluno_endereco_EnderecoRetornoId",
+                        column: x => x.EnderecoRetornoId,
+                        principalTable: "endereco",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_aluno_usuario_ResponsavelId",
+                        column: x => x.ResponsavelId,
+                        principalTable: "usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_aluno_EmpresaId",
+                table: "aluno",
+                column: "EmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_aluno_EnderecoDestinoId",
+                table: "aluno",
+                column: "EnderecoDestinoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_aluno_EnderecoPartidaId",
+                table: "aluno",
+                column: "EnderecoPartidaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_aluno_EnderecoRetornoId",
+                table: "aluno",
+                column: "EnderecoRetornoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_aluno_ResponsavelId",
+                table: "aluno",
+                column: "ResponsavelId");
+        }
+    }
+}
